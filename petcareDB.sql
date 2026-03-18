6b1628e07256a6a8dcda3b19310dffa58e60f287
Alerenca <alerenuncio@gmail.com>
2026-03-18 02:32:51 +0100

    LA BD

--- petcareDB.sql
+++ petcareDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-03-2026 a las 14:35:54
+-- Tiempo de generación: 18-03-2026 a las 02:32:33
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,6 +24,156 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `anuncios`
+--
+
+CREATE TABLE `anuncios` (
+  `id_anuncio` int(11) NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `tipo_mascota` enum('perro','gato','roedor','reptil','pez','ave','otro') NOT NULL,
+  `precio_hora` int(11) NOT NULL,
+  `tipo_servicio` enum('cuidador','transporte','entrenador') NOT NULL,
+  `disponibilidad` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT 1,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `articulos`
+--
+
+CREATE TABLE `articulos` (
+  `id_articulo` int(11) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `cuerpo` text NOT NULL,
+  `fecha_publicación` datetime NOT NULL,
+  `imagen` longblob DEFAULT NULL,
+  `visualizaciones` int(11) NOT NULL DEFAULT 0,
+  `activo` tinyint(1) NOT NULL DEFAULT 1,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cartilla_medica`
+--
+
+CREATE TABLE `cartilla_medica` (
+  `id_cartilla` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT 1,
+  `id_mascota` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `chats`
+--
+
+CREATE TABLE `chats` (
+  `id_chat` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT 1,
+  `id_anuncio` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `chat_usuario`
+--
+
+CREATE TABLE `chat_usuario` (
+  `id_chat_usuario` int(11) NOT NULL,
+  `id_chat` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cita_veterinaria`
+--
+
+CREATE TABLE `cita_veterinaria` (
+  `id_cita` int(11) NOT NULL,
+  `clinica` varchar(100) NOT NULL,
+  `observaciones` text NOT NULL,
+  `fecha` datetime NOT NULL,
+  `diagnostico` text DEFAULT NULL,
+  `id_cartilla` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id_comentario` int(11) NOT NULL,
+  `contenido` text NOT NULL,
+  `fecha_publicacion` datetime NOT NULL,
+  `id_foro` int(11) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `condicion_medica`
+--
+
+CREATE TABLE `condicion_medica` (
+  `id_condicion` int(11) NOT NULL,
+  `tipo` enum('enfermedad','alergia','condicion') NOT NULL,
+  `estado` enum('activa','superada') NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `fecha_diagnostico` date NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `id_cartilla` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empresas`
+--
+
+CREATE TABLE `empresas` (
+  `id_empresa` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `contraseña` varchar(255) NOT NULL,
+  `CIF` varchar(50) NOT NULL,
+  `telefono_contacto` int(11) NOT NULL,
+  `tipo` enum('clinica_veterinaria','hotel') NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `ubicacion` varchar(255) DEFAULT NULL,
+  `foto` longblob DEFAULT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `foros`
+--
+
+CREATE TABLE `foros` (
+  `id_foro` int(11) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `categoría` varchar(100) DEFAULT NULL,
+  `descripcion` text NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT 1,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `mascotas`
 --
 
@@ -35,12 +185,214 @@ CREATE TABLE `mascotas` (
   `raza` varchar(50) NOT NULL,
   `peso` float NOT NULL,
   `foto` longblob DEFAULT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT 1,
   `id_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensajes`
+--
+
+CREATE TABLE `mensajes` (
+  `id_mensaje` int(11) NOT NULL,
+  `contenido` text NOT NULL,
+  `fecha` datetime NOT NULL,
+  `leido` tinyint(1) NOT NULL DEFAULT 0,
+  `id_chat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `notificaciones`
+--
+
+CREATE TABLE `notificaciones` (
+  `id_notificacion` int(11) NOT NULL,
+  `mensaje` text NOT NULL,
+  `leida` tinyint(1) NOT NULL DEFAULT 0,
+  `fecha_creacion` datetime NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reportes`
+--
+
+CREATE TABLE `reportes` (
+  `id_reporte` int(11) NOT NULL,
+  `motivo` enum('spam','lenguaje_ofensivo','contenido_inapropiado','informacion_falsa') NOT NULL,
+  `estado` enum('aceptado','rechazado','pendiente') NOT NULL,
+  `fecha` datetime NOT NULL,
+  `id_autor` int(11) NOT NULL,
+  `id_usuario_reportado` int(11) DEFAULT NULL,
+  `id_foro` int(11) DEFAULT NULL,
+  `id_comentario` int(11) DEFAULT NULL,
+  `id_valoracion` int(11) DEFAULT NULL
+) ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reservas`
+--
+
+CREATE TABLE `reservas` (
+  `id_reserva` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tratamientos`
+--
+
+CREATE TABLE `tratamientos` (
+  `id_tratamiento` int(11) NOT NULL,
+  `medicamento` varchar(100) NOT NULL,
+  `dosis` varchar(150) NOT NULL,
+  `frecuencia` varchar(150) NOT NULL,
+  `fecha_inicio` datetime NOT NULL,
+  `fecha_fin` datetime NOT NULL,
+  `observaciones` text DEFAULT NULL,
+  `id_cartilla` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre_usuario` varchar(50) NOT NULL,
+  `nombre_completo` varchar(50) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `ciudad` varchar(50) DEFAULT NULL,
+  `pais` varchar(50) DEFAULT NULL,
+  `telefono` int(11) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `contraseña` varchar(255) NOT NULL,
+  `bio` text DEFAULT NULL,
+  `rol` enum('user','admin') NOT NULL DEFAULT 'user',
+  `codigo_postal` int(11) DEFAULT NULL,
+  `genero` enum('hombre','mujer','otro','no_decirlo') DEFAULT NULL,
+  `trabajo` varchar(50) DEFAULT NULL,
+  `foto` longblob DEFAULT NULL,
+  `ban` tinyint(1) NOT NULL DEFAULT 0,
+  `suspendido` tinyint(1) NOT NULL DEFAULT 0,
+  `activo` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vacunas`
+--
+
+CREATE TABLE `vacunas` (
+  `id_vacuna` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `fecha_administracion` date NOT NULL,
+  `observaciones` text DEFAULT NULL,
+  `id_cartilla` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `valoraciones`
+--
+
+CREATE TABLE `valoraciones` (
+  `id_valoracion` int(11) NOT NULL,
+  `puntuacion` int(11) NOT NULL,
+  `comentario` text DEFAULT NULL,
+  `id_autor` int(11) NOT NULL,
+  `id_destinatario` int(11) DEFAULT NULL,
+  `id_empresa` int(11) DEFAULT NULL
+) ;
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `anuncios`
+--
+ALTER TABLE `anuncios`
+  ADD PRIMARY KEY (`id_anuncio`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `articulos`
+--
+ALTER TABLE `articulos`
+  ADD PRIMARY KEY (`id_articulo`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `cartilla_medica`
+--
+ALTER TABLE `cartilla_medica`
+  ADD PRIMARY KEY (`id_cartilla`),
+  ADD KEY `id_mascota` (`id_mascota`);
+
+--
+-- Indices de la tabla `chats`
+--
+ALTER TABLE `chats`
+  ADD PRIMARY KEY (`id_chat`),
+  ADD KEY `id_anuncio` (`id_anuncio`);
+
+--
+-- Indices de la tabla `chat_usuario`
+--
+ALTER TABLE `chat_usuario`
+  ADD PRIMARY KEY (`id_chat_usuario`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_chat` (`id_chat`);
+
+--
+-- Indices de la tabla `cita_veterinaria`
+--
+ALTER TABLE `cita_veterinaria`
+  ADD PRIMARY KEY (`id_cita`),
+  ADD KEY `cita_veterinaria_ibfk_1` (`id_cartilla`);
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id_comentario`),
+  ADD KEY `id_foro` (`id_foro`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `condicion_medica`
+--
+ALTER TABLE `condicion_medica`
+  ADD KEY `id_cartilla` (`id_cartilla`);
+
+--
+-- Indices de la tabla `empresas`
+--
+ALTER TABLE `empresas`
+  ADD PRIMARY KEY (`id_empresa`),
+  ADD UNIQUE KEY `correo` (`correo`),
+  ADD UNIQUE KEY `CIF` (`CIF`);
+
+--
+-- Indices de la tabla `foros`
+--
+ALTER TABLE `foros`
+  ADD PRIMARY KEY (`id_foro`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `mascotas`
@@ -50,8 +402,124 @@ ALTER TABLE `mascotas`
   ADD KEY `id_usuario` (`id_usuario`);
 
 --
+-- Indices de la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  ADD PRIMARY KEY (`id_mensaje`),
+  ADD KEY `id_chat` (`id_chat`);
+
+--
+-- Indices de la tabla `notificaciones`
+--
+ALTER TABLE `notificaciones`
+  ADD PRIMARY KEY (`id_notificacion`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `reportes`
+--
+ALTER TABLE `reportes`
+  ADD PRIMARY KEY (`id_reporte`),
+  ADD KEY `id_autor` (`id_autor`),
+  ADD KEY `id_usuario_reportado` (`id_usuario_reportado`),
+  ADD KEY `id_comentario` (`id_comentario`),
+  ADD KEY `id_foro` (`id_foro`),
+  ADD KEY `id_valoracion` (`id_valoracion`);
+
+--
+-- Indices de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  ADD PRIMARY KEY (`id_reserva`);
+
+--
+-- Indices de la tabla `tratamientos`
+--
+ALTER TABLE `tratamientos`
+  ADD PRIMARY KEY (`id_tratamiento`),
+  ADD KEY `tratamientos_ibfk_1` (`id_cartilla`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `nombre_usuario` (`nombre_usuario`),
+  ADD UNIQUE KEY `correo` (`correo`);
+
+--
+-- Indices de la tabla `vacunas`
+--
+ALTER TABLE `vacunas`
+  ADD PRIMARY KEY (`id_vacuna`),
+  ADD KEY `vacunas_ibfk_1` (`id_cartilla`);
+
+--
+-- Indices de la tabla `valoraciones`
+--
+ALTER TABLE `valoraciones`
+  ADD PRIMARY KEY (`id_valoracion`),
+  ADD KEY `id_autor` (`id_autor`),
+  ADD KEY `id_destinatario` (`id_destinatario`),
+  ADD KEY `id_empresa` (`id_empresa`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `anuncios`
+--
+ALTER TABLE `anuncios`
+  MODIFY `id_anuncio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `articulos`
+--
+ALTER TABLE `articulos`
+  MODIFY `id_articulo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cartilla_medica`
+--
+ALTER TABLE `cartilla_medica`
+  MODIFY `id_cartilla` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `chats`
+--
+ALTER TABLE `chats`
+  MODIFY `id_chat` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `chat_usuario`
+--
+ALTER TABLE `chat_usuario`
+  MODIFY `id_chat_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cita_veterinaria`
+--
+ALTER TABLE `cita_veterinaria`
+  MODIFY `id_cita` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `empresas`
+--
+ALTER TABLE `empresas`
+  MODIFY `id_empresa` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `foros`
+--
+ALTER TABLE `foros`
+  MODIFY `id_foro` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `mascotas`
@@ -60,14 +528,160 @@ ALTER TABLE `mascotas`
   MODIFY `id_mascota` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  MODIFY `id_mensaje` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `notificaciones`
+--
+ALTER TABLE `notificaciones`
+  MODIFY `id_notificacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `reportes`
+--
+ALTER TABLE `reportes`
+  MODIFY `id_reporte` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tratamientos`
+--
+ALTER TABLE `tratamientos`
+  MODIFY `id_tratamiento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `vacunas`
+--
+ALTER TABLE `vacunas`
+  MODIFY `id_vacuna` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `valoraciones`
+--
+ALTER TABLE `valoraciones`
+  MODIFY `id_valoracion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `anuncios`
+--
+ALTER TABLE `anuncios`
+  ADD CONSTRAINT `anuncios_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `articulos`
+--
+ALTER TABLE `articulos`
+  ADD CONSTRAINT `articulos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `cartilla_medica`
+--
+ALTER TABLE `cartilla_medica`
+  ADD CONSTRAINT `cartilla_medica_ibfk_1` FOREIGN KEY (`id_mascota`) REFERENCES `mascotas` (`id_mascota`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `chats`
+--
+ALTER TABLE `chats`
+  ADD CONSTRAINT `chats_ibfk_1` FOREIGN KEY (`id_anuncio`) REFERENCES `anuncios` (`id_anuncio`) ON DELETE SET NULL ON UPDATE SET NULL;
+
+--
+-- Filtros para la tabla `chat_usuario`
+--
+ALTER TABLE `chat_usuario`
+  ADD CONSTRAINT `chat_usuario_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `chat_usuario_ibfk_2` FOREIGN KEY (`id_chat`) REFERENCES `chats` (`id_chat`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `cita_veterinaria`
+--
+ALTER TABLE `cita_veterinaria`
+  ADD CONSTRAINT `cita_veterinaria_ibfk_1` FOREIGN KEY (`id_cartilla`) REFERENCES `cartilla_medica` (`id_cartilla`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`id_foro`) REFERENCES `foros` (`id_foro`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE SET NULL ON UPDATE SET NULL;
+
+--
+-- Filtros para la tabla `condicion_medica`
+--
+ALTER TABLE `condicion_medica`
+  ADD CONSTRAINT `condicion_medica_ibfk_1` FOREIGN KEY (`id_cartilla`) REFERENCES `cartilla_medica` (`id_cartilla`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `foros`
+--
+ALTER TABLE `foros`
+  ADD CONSTRAINT `foros_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `mascotas`
 --
 ALTER TABLE `mascotas`
   ADD CONSTRAINT `mascotas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  ADD CONSTRAINT `mensajes_ibfk_1` FOREIGN KEY (`id_chat`) REFERENCES `chats` (`id_chat`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `notificaciones`
+--
+ALTER TABLE `notificaciones`
+  ADD CONSTRAINT `notificaciones_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `reportes`
+--
+ALTER TABLE `reportes`
+  ADD CONSTRAINT `reportes_ibfk_1` FOREIGN KEY (`id_autor`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reportes_ibfk_2` FOREIGN KEY (`id_usuario_reportado`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reportes_ibfk_3` FOREIGN KEY (`id_comentario`) REFERENCES `comentarios` (`id_comentario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reportes_ibfk_4` FOREIGN KEY (`id_foro`) REFERENCES `foros` (`id_foro`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reportes_ibfk_5` FOREIGN KEY (`id_valoracion`) REFERENCES `valoraciones` (`id_valoracion`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tratamientos`
+--
+ALTER TABLE `tratamientos`
+  ADD CONSTRAINT `tratamientos_ibfk_1` FOREIGN KEY (`id_cartilla`) REFERENCES `cartilla_medica` (`id_cartilla`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `vacunas`
+--
+ALTER TABLE `vacunas`
+  ADD CONSTRAINT `vacunas_ibfk_1` FOREIGN KEY (`id_cartilla`) REFERENCES `cartilla_medica` (`id_cartilla`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `valoraciones`
+--
+ALTER TABLE `valoraciones`
+  ADD CONSTRAINT `valoraciones_ibfk_1` FOREIGN KEY (`id_autor`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `valoraciones_ibfk_2` FOREIGN KEY (`id_destinatario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `valoraciones_ibfk_3` FOREIGN KEY (`id_empresa`) REFERENCES `empresas` (`id_empresa`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
