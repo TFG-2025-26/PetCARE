dc5fe7381880adc766920e67d69e1a7d2f2740c0
Igcabr01 <igcabr01@ucm.es>
2026-04-03 18:14:53 +0200

    gestión de foros parcialmente terminada

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -7,5 +7,10 @@ const adminController = require('../controllers/adminController');
 const { isAuthenticated } = require('../middlewares/authMiddleware');
 
 router.get('/adminPanel', isAuthenticated, adminController.getAdminPanel);
+router.get('/adminPanel/gestionReportes', isAuthenticated, adminController.getGestionReportes);
+router.get('/adminPanel/gestionReportes/filtrar', isAuthenticated, adminController.filtrarReportes);
+router.get('/adminPanel/gestionReportes/:id_reporte', isAuthenticated, adminController.getDetalleReporte);
+router.post('/adminPanel/gestionReportes/:id_reporte/acciones/:accion', isAuthenticated, adminController.aplicarAccionReporte);
+router.get('/adminPanel/gestionReportes/:id_reporte/editarAccion', isAuthenticated, adminController.editarAccionReporte);
 
 module.exports = router;
